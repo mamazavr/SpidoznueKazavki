6370eef052e11a756695afd6db9b127361170eb9
serGAY <116800609+mamazavr@users.noreply.github.com>
2023-12-29 11:45:32 +0200

    Add files via upload

---
+++ app/Models/Folder.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class Folder extends \Core\Model
+{
+    protected static string|null $tableName = 'folders';
+}
---
+++ app/Views/Auth/AuthValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Validators\Users;
+
+class AuthValidator extends Base
+{
+    const DEFAULT_MESSAGE = 'Email or password is incorrect';
+
+    protected array $rules = [
+        'email' => '/^[a-zA-Z0-9.!#$%&\'*+\/\?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i',
+        'password' => '/[a-zA-Z0-9.!#$%&\'*+\/\?^_`{|}~-]{8,}/',
+    ];
+
+    protected array $errors = [
+        'email' => self::DEFAULT_MESSAGE,
+        'password' => self::DEFAULT_MESSAGE
+    ];
+
+    public function validate(array $fields = []): bool
+    {
+        $result = [
+            parent::validate($fields),
+            $this->checkEmailOnExists($fields['email'], false, self::DEFAULT_MESSAGE)
+        ];
+
+        return !in_array(false, $result);
+    }
+}
